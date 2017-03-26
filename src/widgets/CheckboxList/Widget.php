ec567169b34800901d62661b577dc3546ace923f
Vladimir <backend@div26.ru>
2017-03-26 14:52:53 +0300

    сделал возможность представления списка чекбоксов несколькими колонками

--- src/widgets/CheckboxList/Widget.php
+++ src/widgets/CheckboxList/Widget.php
@@ -3,25 +3,27 @@ namespace janisto\ycm\widgets\CheckboxList;
 
 use yii\base\Widget as BaseWidget;
 use yii\bootstrap\Html;
+use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
 
 /**
  * @author Владимир
  */
-class Widget extends BaseWidget {
-	public $model;
-	
-	public $attribute;
-	
-	/**
-	 * @var string поле с названием в связанной модели
-	 */
-	public $nameAttribute = 'name';
-	
-	/**
-	 * @var string название связи
-	 */
-	public $relationName;
+class Widget extends BaseWidget
+{
+    public $model;
+
+    public $attribute;
+
+    /**
+     * @var string поле с названием в связанной модели
+     */
+    public $nameAttribute = 'name';
+
+    /**
+     * @var string название связи
+     */
+    public $relationName;
 
     /**
      * @var callable функция, которая проверяет, должен ли быть отображён чекбокс или нет
@@ -29,44 +31,56 @@ class Widget extends BaseWidget {
      */
     public $checkItem;
 
-	public function init() {
-		if(!$this->relationName) {
-			$this->relationName = $this->attribute;
-		}
-		
-		parent::init();
-	}
-	
-	public function run() {
-		$relation = $this->model->getRelation($this->relationName);
-		$relationClass = $relation->modelClass;
-		$values = $relationClass::find()->all();
+    /**
+     * @var int число колонок в виджете
+     */
+    public $columnsCount = 1;
 
-		if (is_callable($this->checkItem)) {
-		    foreach ($values as $k => $v) {
+    public function init()
+    {
+        if (!$this->relationName) {
+            $this->relationName = $this->attribute;
+        }
+
+        parent::init();
+    }
+
+    public function run()
+    {
+        /** @var ActiveRecord $model */
+        $model = $this->model;
+        $relation = $model->getRelation($this->relationName);
+        $relationClass = $relation->modelClass;
+        $values = $relationClass::find()->all();
+
+        if (is_callable($this->checkItem)) {
+            foreach ($values as $k => $v) {
                 if (!call_user_func($this->checkItem, $v)) {
                     unset($values[$k]);
                 }
             }
         }
 
-		$selected = ArrayHelper::getColumn($this->model->{$this->attribute}, 'id');
-		
-		/*$result = '';
-		foreach($properties as $prop) {
-			if(isset($values[$prop->id])) $value = $values[$prop->id]->id;
-			else $value = null;
-			$result .= $this->createRow($prop, $value);
-		}*/
-		
-		$name = Html::getInputName($this->model, $this->attribute);
-		
-		return Html::checkboxList($name, $selected, ArrayHelper::map($values, 'id', $this->nameAttribute), [
-			'separator' => '<br>'
-		]);
-	}
-	
-	protected function createRow($prop, $value = null) {
-		
-	}
+        $selected = ArrayHelper::getColumn($this->model->{$this->attribute}, 'id');
+
+        $name = Html::getInputName($this->model, $this->attribute);
+
+        $columnsCount = min(max($this->columnsCount, 1), 10);
+        $countInColumn = ceil(count($values) / $columnsCount);
+        $columns = array_chunk($values, $countInColumn);
+
+        $result = '';
+        foreach ($columns as $column) {
+            $columnContent = Html::checkboxList(
+                $name,
+                $selected,
+                ArrayHelper::map($column, 'id', $this->nameAttribute),
+                [
+                    'separator' => '<br>',
+                ]);
+            $result .= '<div style="display: inline-block; vertical-align: top; width: ' . (100 / $columnsCount) . '%">' . $columnContent . '</div>';
+        }
+
+        return '<div>' . $result . '</div>';
+    }
 }
