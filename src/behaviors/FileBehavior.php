d7c924982fc074f512222fa23fb7e1e1df113f57
Vladimir <backend@div26.ru>
2017-04-04 14:08:01 +0300

    сделал метод для сохранения загруженного файла

--- src/behaviors/FileBehavior.php
+++ src/behaviors/FileBehavior.php
@@ -5,6 +5,8 @@ namespace janisto\ycm\behaviors;
 use Yii;
 use yii\base\Behavior;
 use yii\base\InvalidConfigException;
+use yii\web\UploadedFile;
+use yii\helpers\FileHelper;
 
 class FileBehavior extends Behavior
 {
@@ -23,11 +25,6 @@ class FileBehavior extends Behavior
     public function init()
     {
         parent::init();
-
-        if ($this->folderName === null) {
-			$module = Yii::$app->getModule('ycm');
-            $this->folderName = $module->getModelName($this->owner);
-        }
 
         if ($this->uploadPath === null) {
             $this->uploadPath = Yii::getAlias('@uploadPath');
@@ -51,7 +48,7 @@ class FileBehavior extends Behavior
 
         if ($model->hasAttribute($attribute) && !empty($model->$attribute)) {
             $file = $model->$attribute;
-            $path = $this->uploadPath . DIRECTORY_SEPARATOR . strtolower($this->folderName) . DIRECTORY_SEPARATOR . strtolower($attribute);
+            $path = $this->uploadPath . DIRECTORY_SEPARATOR . strtolower($this->getFolderName()) . DIRECTORY_SEPARATOR . strtolower($attribute);
             return $path . DIRECTORY_SEPARATOR . $file;
         }
         return false;
@@ -70,7 +67,7 @@ class FileBehavior extends Behavior
 
         if ($model->hasAttribute($attribute) && !empty($model->$attribute)) {
             $file = $model->$attribute;
-            $path = $this->uploadUrl . '/' . strtolower($this->folderName) . '/' . strtolower($attribute);
+            $path = $this->uploadUrl . '/' . strtolower($this->getFolderName()) . '/' . strtolower($attribute);
             return $path . '/' . $file;
         }
         return false;
@@ -95,8 +92,52 @@ class FileBehavior extends Behavior
         return false;
     }
 	
+	protected function getFolderName()
+	{
+		if (is_null($this->folderName)) {
+			$module = Yii::$app->modules['ycm'];
+			return $module->getModelName($this->owner);
+		} else {
+			return $this->folderName;
+		}
+	}
+	
 	public function getSavePath($attribute)
 	{
-		return $this->uploadPath . DIRECTORY_SEPARATOR . strtolower($this->folderName) . DIRECTORY_SEPARATOR . strtolower($attribute);
+		return $this->uploadPath . DIRECTORY_SEPARATOR . strtolower($this->getFolderName()) . DIRECTORY_SEPARATOR . strtolower($attribute);
+	}
+	
+	/**
+	 * сохранение загруженного файла
+	 * 
+	 * @param UploadedFile|string имя файла из формы или объект загруженного файла
+	 * @param string $attribute
+	 * @return string
+	 */
+	public function saveUploadedFile($file, $attribute)
+	{
+		if (is_string($file)) {
+			$file = UploadedFile::getInstanceByName($file);
+		} elseif (!($file instanceof UploadedFile)) {
+			throw new InvalidConfigException();
+		}
+		
+		$module = Yii::$app->modules['ycm'];
+		
+		$attributePath = $module->getAttributePath($this->getFolderName(), $attribute);
+		
+		$fileName = md5($attribute . time() . uniqid(rand(), true)) . '.' . $file->extension;
+		if (!is_dir($attributePath)) {
+			if (!FileHelper::createDirectory($attributePath, $module->uploadPermissions)) {
+				throw new InvalidConfigException('Could not create folder "' . $attributePath . '". Make sure "uploads" folder is writable.');
+			}
+		}
+		$path = $attributePath . DIRECTORY_SEPARATOR . $fileName;
+		if (file_exists($path) || !$file->saveAs($path)) {
+			throw new ServerErrorHttpException('Could not save file or file exists: ' . $path);
+		}
+		$this->owner->$attribute = $fileName;
+		
+		return $fileName;
 	}
 }
