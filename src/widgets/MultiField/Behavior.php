8e1a02b63b029219e056fa52655cf45d0c3620f4
Vladimir <backend@div26.ru>
2017-04-23 15:13:25 +0300

    сделал метод для получения списка доп полей в поведении

--- src/widgets/MultiField/Behavior.php
+++ src/widgets/MultiField/Behavior.php
@@ -27,26 +27,48 @@ class Behavior extends BaseBehavior {
 			return false;
 		}
 		
-		$model = $this->owner;
-		
-		$fields = $model->attributeWidgets();
-		
-		foreach($fields as $field) {
-			if($field[1] == 'widget' && isset($field['widgetClass']) && $field['widgetClass'] == 'janisto\ycm\widgets\MultiField\Widget') {
-				if(isset($field['dataClass'])) $dataClass = $field['dataClass'];
-				else $dataClass = 'janisto\ycm\widgets\MultiField\DataAttribute';
-				$data = new $dataClass([
-					'model' => $model,
-					'attribute' => $field[0]
-				]);
-				
-				$data->save();
-			}
-		}
+		$dataObjects = $this->getDataObjects();
+		foreach ($dataObjects as $obj) {
+		    $obj->save();
+        }
 		return true;
 	}
 	
 	public function onUnsafeAttribute($name, $value) {
 		
 	}
+
+	public function getFields($attribute)
+    {
+        $dataObjects = $this->getDataObjects();
+        foreach ($dataObjects as $obj) {
+            if ($obj->attribute == $attribute) {
+                return $obj->getList();
+            }
+        }
+
+        return [];
+    }
+
+    protected function getDataObjects()
+    {
+        $model = $this->owner;
+
+        $fields = $model->attributeWidgets();
+
+        $result = [];
+
+        foreach($fields as $field) {
+            if($field[1] == 'widget' && isset($field['widgetClass']) && $field['widgetClass'] == 'janisto\ycm\widgets\MultiField\Widget') {
+                if(isset($field['dataClass'])) $dataClass = $field['dataClass'];
+                else $dataClass = 'janisto\ycm\widgets\MultiField\DataAttribute';
+                $result[] = new $dataClass([
+                    'model' => $model,
+                    'attribute' => $field[0]
+                ]);
+            }
+        }
+
+        return $result;
+    }
 }
